81d5a254e2fac6c8c5ce3a0894779259380805fa
Jonas Pammer <opensource@jonaspammer.at>
2022-04-22 12:10:05 +0200

    chore: create 'skeleton' for single module python project setup
    primary sauce: anthony's project ;). will make my own cookiecutter soon potentially

---
+++ ansible-roles.py
@@ -0,0 +1,16 @@
+from __future__ import annotations
+
+from typing import Sequence
+
+# import argparse
+
+
+def main(argv: Sequence[str] | None = None) -> int:
+    # parser = argparse.ArgumentParser()
+
+    retv = 0
+    return retv
+
+
+if __name__ == "__main__":
+    raise SystemExit(main())
