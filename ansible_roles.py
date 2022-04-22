0b2084580a752a712d6d44461f8848d613acfd35
Jonas Pammer <opensource@jonaspammer.at>
2022-04-22 12:26:40 +0200

    chore: remove unused boilerplate

--- ansible_roles.py
+++ ansible_roles.py
@@ -2,12 +2,8 @@ from __future__ import annotations
 
 from typing import Sequence
 
-# import argparse
-
 
 def main(argv: Sequence[str] | None = None) -> int:
-    # parser = argparse.ArgumentParser()
-
     retv = 0
     return retv
 
