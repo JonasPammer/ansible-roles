d03eb56ca944cea4e689eab0c77535b90c97eeb5
Jonas Pammer <opensource@jonaspammer.at>
2022-07-02 22:42:33 +0200

    refactor: factor-out things from ansible_roles.py to utils.py
    add docstrings
    transform variables into module-global variables
    
    note: i am not yet fully confident or knowledged in sphinx cross references as i never generated my docs with sphinx,
    nevertheless i try to use sphinx docformat (default for intellij idea auto-generation and settable using extension for vscode)

--- ansible_roles/ansible_roles.py
+++ ansible_roles/ansible_roles.py
@@ -1,60 +1,13 @@
 from __future__ import annotations
 
-import json
-from typing import Any
 from typing import Sequence
 
-import attrs
-import diskcache
-import yaml
-from github import Github
-from github import GithubException
 from jinja2 import Environment
 from jinja2 import FileSystemLoader
 from jinja2 import select_autoescape
-from rich.console import Console
 from rich.traceback import install as install_rich_traceback
 
-console = Console(width=240)
-github_api = Github()
-# TODO make the github action cache the cache this directory for its future runs
-# note: cache is used solely to avoid github api rate limits
-cache = diskcache.Cache(".ansible_roles_diskcache")
-
-
-# TODO seperate the fetched variables into an own object (without any logic)
-#      and cache that one
-#      caching entire class is no good
-@attrs.define
-class AnsibleRole:
-    repo_name: str
-    repo_pull_url: str
-    galaxy_owner: str = "jonaspammer"
-    requirements_yml: dict[str, Any] = {}
-    meta_yml: dict[str, Any] = {}
-
-    computed_dependencies: list[str] = attrs.field(
-        default=attrs.Factory(list)
-    )  # list of galaxy_role_name's
-
-    @property
-    def role_name(self) -> str:
-        return self.repo_name.replace("ansible-role-", "")
-
-    @property
-    def galaxy_role_name(self) -> str:
-        return self.galaxy_owner + "." + self.role_name
-
-    def get_dependency_color(self) -> str:
-        layer_colors = {
-            0: "green",
-            1: "yellow",
-            2: "orange",
-            3: "red",
-            4: "purple",
-            5: "blue",
-        }
-        return layer_colors[len(self.computed_dependencies)]
+from ansible_roles import utils
 
 
 def main(argv: Sequence[str] | None = None) -> int:
@@ -64,83 +17,22 @@ def main(argv: Sequence[str] | None = None) -> int:
         loader=FileSystemLoader("templates"),
         autoescape=select_autoescape(),
     )
-
-    with open("all-repos-in.json") as f:
-        all_repos_in = json.load(f)
-    # TODO extend functionality to sort above json file
-
-    # initialize all_roles
-    all_roles: dict[str, AnsibleRole] = {}
-    for key in all_repos_in:
-        role = AnsibleRole(repo_name=key, repo_pull_url=all_repos_in[key])
-        if not role.repo_name.startswith("ansible-role"):
-            console.log(f"{role.repo_name} is not an ansible role, skipping...")
-            continue
-        if cache.get(role.galaxy_role_name) is not None:
-            console.log(f"{role.galaxy_role_name} exists in cache, skipping...")
-            all_roles[role.galaxy_role_name] = cache.get(role.galaxy_role_name)
-            continue
-
-        console.log(
-            f"querying additional information of {role.galaxy_role_name} "
-            f"using github api..."
-        )
-        repo = github_api.get_repo(f"JonasPammer/{role.repo_name}")
-
-        try:
-            role.requirements_yml = yaml.safe_load(
-                repo.get_contents("requirements.yml").decoded_content
-            )
-            role.meta_yml = yaml.safe_load(
-                repo.get_contents("meta/main.yml").decoded_content
-            )
-        # allow for yet-just-created / WIP ansible roles of mine
-        except GithubException as ex:
-            if "empty" not in str(ex):
-                raise ex
-
-        cache.set(key=role.galaxy_role_name, value=role, expire=60 * 60)
-        all_roles[role.galaxy_role_name] = role
-
-    # compute values
-    for galaxy_role_name, role in all_roles.items():
-        if "roles" not in role.requirements_yml:
-            continue  # no dependencies
-        for role_req in role.requirements_yml["roles"]:
-            if "__not_mandatory_to_role_itself" not in role_req:
-                role.computed_dependencies.append(role_req["name"])
-            # role.dependencies_not_mandatory_to_role_itself.append(role_req["name"])
+    utils.init()
 
     with open("README.adoc", "w", encoding="utf-8") as f:
-        f.write(env.get_template("README.adoc.jinja2").render(all_roles=all_roles))
+        f.write(
+            env.get_template("README.adoc.jinja2").render(all_roles=utils.all_roles)
+        )
     with open("graphs/dependencies_ALL.dot", "w", encoding="utf-8") as f:
         f.write(
-            env.get_template("dependencies_ALL.dot.jinja2").render(all_roles=all_roles)
+            env.get_template("dependencies_ALL.dot.jinja2").render(
+                all_roles=utils.all_roles
+            )
         )
 
     template_dependencies_single = env.get_template("dependencies_single.dot.jinja2")
-    for input_galaxy_role_name, input_role in all_roles.items():
-
-        def recurse_add_dependencies(
-            role: AnsibleRole, _tmp_dict: dict[str, AnsibleRole] | None = None
-        ) -> dict[str, AnsibleRole]:
-            if _tmp_dict is None:
-                _tmp_dict = {}
-
-            _tmp_dict[role.galaxy_role_name] = role
-            for galaxy_dependency in role.computed_dependencies:
-                if galaxy_dependency in all_roles:
-                    role_for_galaxy_dependency = all_roles[galaxy_dependency]
-                else:
-                    role_for_galaxy_dependency = AnsibleRole(
-                        repo_name=galaxy_dependency.split(".")[1],
-                        repo_pull_url="",
-                        galaxy_owner=galaxy_dependency.split(".")[0],
-                    )
-                recurse_add_dependencies(role_for_galaxy_dependency, _tmp_dict)
-            return _tmp_dict
-
-        filtered_roles = recurse_add_dependencies(input_role)
+    for input_galaxy_role_name, input_role in utils.all_roles.items():
+        filtered_roles = utils.recurse_add_dependencies(input_role)
 
         with open(f"graphs/dependencies_{input_role.role_name}.dot", "w") as f:
             f.write(
---
+++ ansible_roles/utils.py
@@ -0,0 +1,157 @@
+
+import attrs
+import diskcache
+from github import Github
+from rich.console import Console
+from github import GithubException
+from typing import Any
+import yaml
+import json
+
+
+console = Console(width=240)
+github_api = Github()
+all_roles: dict[str, "AnsibleRole"] = {}
+"""
+A dictionary of all ansible roles found in `all-repos-in.json` 
+in which the key is the `galaxy_role_name`.
+
+See :func:`utils.init_all_roles`
+"""
+# TODO make the github action cache the cache this directory for its future runs
+# note: cache is used solely to avoid github api rate limits
+__all_roles_cache = diskcache.Cache(".ansible_roles_diskcache")
+
+# TODO seperate the fetched variables into an own object (without any logic)
+#      and cache that one
+#      caching entire class is no good
+@attrs.define
+class AnsibleRole:
+    repo_name: str
+    """ repository name as denoted in all-repos-in.json """
+    repo_pull_url: str
+    """ repository pull URI as denoted in all-repos-in.json """
+    galaxy_owner: str = "jonaspammer"
+    requirements_yml: dict[str, Any] = {}
+    """ decoded content of this role's `requirements.yml` file """
+    meta_yml: dict[str, Any] = {}
+    """ decoded content of this role's `meta/meta.yml` file """
+
+    computed_dependencies: list[str] = attrs.field(
+        default=attrs.Factory(list)
+    )
+    """ list of `galaxy_role_name`'s """
+    
+    @property
+    def role_name(self) -> str:
+        """Utility function to get the actual name of the role.
+
+        :return: `repo_name` with removed slug/repository-prefix (e.g. `ansible-role-`)
+        """
+        return self.repo_name.replace("ansible-role-", "")
+
+    @property
+    def galaxy_role_name(self) -> str:
+        """Utility function to retun this role's fully qualified name.
+
+        :return: This role's fully qualified name in format `galaxy_owner.role_name`.
+        """
+        return self.galaxy_owner + "." + self.role_name
+
+    def get_dependency_color(self) -> str:
+        """
+        :return: Named color understood by Graphviz.
+        """
+        layer_colors = {
+            0: "green",
+            1: "yellow",
+            2: "orange",
+            3: "red",
+            4: "purple",
+            5: "blue",
+        }
+        return layer_colors[len(self.computed_dependencies)]
+
+
+def init_all_roles() -> None:
+    """
+    Initializes the global module variable `all_roles`.
+    """
+    with open("all-repos-in.json") as f:
+        all_repos_in = json.load(f)
+
+    # initialize
+    for key in all_repos_in:
+        role = AnsibleRole(repo_name=key, repo_pull_url=all_repos_in[key])
+        if not role.repo_name.startswith("ansible-role"):
+            console.log(f"{role.repo_name} is not an ansible role, skipping...")
+            continue
+        if __all_roles_cache.get(role.galaxy_role_name) is not None:
+            console.log(f"{role.galaxy_role_name} exists in cache, skipping...")
+            all_roles[role.galaxy_role_name] = __all_roles_cache.get(role.galaxy_role_name)
+            continue
+
+        console.log(
+            f"querying additional information of {role.galaxy_role_name} "
+            f"using github api..."
+        )
+        repo = github_api.get_repo(f"JonasPammer/{role.repo_name}")
+
+        try:
+            role.requirements_yml = yaml.safe_load(
+                repo.get_contents("requirements.yml").decoded_content
+            )
+            role.meta_yml = yaml.safe_load(
+                repo.get_contents("meta/main.yml").decoded_content
+            )
+        # allow for yet-just-created / WIP ansible roles of mine
+        except GithubException as ex:
+            if "empty" not in str(ex):
+                raise ex
+
+        __all_roles_cache.set(key=role.galaxy_role_name, value=role, expire=60 * 60)
+        all_roles[role.galaxy_role_name] = role
+
+    # compute values
+    for galaxy_role_name, role in all_roles.items():
+        if "roles" not in role.requirements_yml:
+            continue  # no dependencies
+        for role_req in role.requirements_yml["roles"]:
+            if "__not_mandatory_to_role_itself" not in role_req:
+                role.computed_dependencies.append(role_req["name"])
+            # role.dependencies_not_mandatory_to_role_itself.append(role_req["name"])
+
+def init() -> None:
+    """ Initialize all Variables global to this module. """
+    init_all_roles()
+
+def recurse_add_dependencies(
+    role: AnsibleRole, 
+    __tmp_dict: dict[str, AnsibleRole] | None = None
+) -> dict[str, AnsibleRole]:
+    """
+    :param role: AnsibleRole which to dig through it's `computed_dependencies`.
+    :return: 
+        A list of all computed soft role dependencies
+        in form of a dict in which the key represent the role's `galaxy_role_name`.
+        
+        Note that a entry in this role may not be a fully qualified AnsibleRole
+        and only contain marginal information if said role is not found 
+        in the global module variable `all_roles`
+    """
+    if __tmp_dict is None:
+        __tmp_dict = {}
+    __tmp_dict[role.galaxy_role_name] = role
+    
+    for galaxy_dependency in role.computed_dependencies:
+        if galaxy_dependency in all_roles:
+            role_for_galaxy_dependency = all_roles[galaxy_dependency]
+        else:
+            role_for_galaxy_dependency = AnsibleRole(
+                repo_name=galaxy_dependency.split(".")[1],
+                repo_pull_url="",
+                galaxy_owner=galaxy_dependency.split(".")[0],
+            )
+        recurse_add_dependencies(role_for_galaxy_dependency, __tmp_dict)
+    return __tmp_dict
+
