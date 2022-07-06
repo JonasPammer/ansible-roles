20701626f5af0c156f6e1df2ebb20658c91221c2
Jonas Pammer <opensource@jonaspammer.at>
2022-07-06 16:01:30 +0200

    chore: add better logging of results for cruft update script

--- ansible_roles_scripts/cruft.py
+++ ansible_roles_scripts/cruft.py
@@ -3,6 +3,7 @@ from __future__ import annotations
 import subprocess
 from pathlib import Path
 
+import attrs
 import click
 
 from ansible_roles import utils
@@ -12,6 +13,13 @@ from ansible_roles_scripts import script_utils
 from ansible_roles_scripts.script_utils import check_conflict_files
 from ansible_roles_scripts.script_utils import COMMIT_MSG
 from ansible_roles_scripts.script_utils import execute
+
+
+@attrs.define
+class CruftUpdateResult:
+    path: Path
+    all_ok: bool = False
+    is_something_to_push: bool = False
 
 
 def check_rejected_files(path: Path) -> bool:
@@ -31,7 +39,9 @@ def check_rejected_files(path: Path) -> bool:
     return False
 
 
-def cruft_update(path: Path, push: bool) -> bool:
+def cruft_update(path: Path, push: bool) -> CruftUpdateResult:
+    retv: CruftUpdateResult = CruftUpdateResult(path=path)
+
     def _is_real_commit_error(ex: subprocess.CalledProcessError) -> bool:
         return not any(
             match in ex.stdout.decode() for match in ["nichts zu", "nothing to"]
@@ -41,7 +51,7 @@ def cruft_update(path: Path, push: bool) -> bool:
     logger.info(f"Start procedure for '{path}'")
 
     if check_rejected_files(path) or check_conflict_files(path):
-        return False
+        return retv
 
     execute(["git", "add", "."], path)
     execute(
@@ -54,7 +64,7 @@ def cruft_update(path: Path, push: bool) -> bool:
 
     execute(["cruft", "update", "-y"], path)
     if check_rejected_files(path) or check_conflict_files(path):
-        return False
+        return retv
     execute(["git", "add", "."], path)
     execute(
         ["git", "commit", "-m", "chore: cruft update", "-m", COMMIT_MSG],
@@ -80,12 +90,14 @@ def cruft_update(path: Path, push: bool) -> bool:
             execute(["git", "push"], path)
             logger.success("Successfully pushed the results.")
         else:
+            retv.is_something_to_push = True
             logger.notice("Not pushing the results.")
     else:
         logger.info("Nothing to push.")
 
+    retv.all_ok = True
     logger.info(f"Successfully ended procedure for '{path}'")
-    return True
+    return retv
 
 
 @click.command(
@@ -104,24 +116,42 @@ def main(push: bool, silent: bool, verbosity: int) -> int:
         return 127
 
     all_repos: list[Path] = script_utils.get_all_cloned_ansible_repositories()
-    not_ok = set()
-    for repo in all_repos:
+    results = {
+        repo_path.name: CruftUpdateResult(path=repo_path) for repo_path in all_repos
+    }
+    for repo_path in all_repos:
         try:
-            ok = cruft_update(repo, push)
-            if not ok:
-                not_ok.add(repo)
+            results[repo_path.name] = cruft_update(repo_path, push)
         except subprocess.CalledProcessError:
             # initially catched by function,
             # thrown again to abort that function
-            not_ok.add(repo)
             retv = 1
             pass
-    if len(not_ok) == 0:
+
+    not_ok_results: list[CruftUpdateResult] = [
+        result for result in results.values() if not result.all_ok
+    ]
+    if len(not_ok_results) == 0:
         logger.success("Sucessfully run procedure on all repositories!")
     else:
         logger.error(
-            f"The following repos did not succeed: "
-            f"{', '.join([repo.name for repo in not_ok])}"
+            f"The following {len(not_ok_results)} repositories "
+            "did not finish the procedure as expected: \n* "
+            + "\n* ".join([repo.path.name for repo in not_ok_results])
+            + "\n"
+            + "Please see the respective sections and their logs for more information."
+        )
+
+    pushable_results: list[CruftUpdateResult] = [
+        result for result in results.values() if result.is_something_to_push
+    ]
+    if len(pushable_results) != 0:
+        logger.notice(
+            f"The following {len(pushable_results)} repositories "
+            "contain unpushed commits: \n* "
+            + "\n* ".join([repo.path.name for repo in pushable_results])
+            + "\n"
+            "Invoke the command again with '--push' to push them!"
         )
 
     return retv
